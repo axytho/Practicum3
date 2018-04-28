b0cc681e6ec5bae0aaf59034a04818cf20b1f79f
axytho <jojojo.jonas888@hotmail.com>
2018-04-28 13:48:59 +0200

    Now make some tests

---
+++ Practicum3/src/testfilesystem/TestItem.java
@@ -0,0 +1,21 @@
+package testfilesystem;
+
+import static org.junit.Assert.*;
+import filesystem.*;
+import org.junit.*;
+
+public class TestItem {
+
+	private static Directory directory1;
+	
+	private static File file1;
+	
+	private static Link link1;
+	
+	@Before
+	public void setUpFixture()	{
+		
+	}
+	
+	
+}
